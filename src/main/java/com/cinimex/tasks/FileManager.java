0a496327d6419ff0bb1c22a2c79644ed25ca5eb9
chemerisov.oleg <hspqwi@gmail.com>
2020-02-02 00:55:32 +0300

    1. Task done 2. Added tests

--- src/main/java/com/cinimex/tasks/FileManager.java
+++ src/main/java/com/cinimex/tasks/FileManager.java
@@ -1,45 +1,128 @@
 package com.cinimex.tasks;
 
+import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FileManager<K, V> {
 
-    private volatile ConcurrentHashMap<Key, V> repository = new ConcurrentHashMap<>();
-    private long timeLeft;
+    private volatile Map<Key, V> repository = new ConcurrentHashMap<>();
+    private long timeToLive;
+    private long checkTime;
 
-    public FileManager(long timeLeft) {
-        this.timeLeft = timeLeft;
+    /**
+     * Default, the check time for deleting "expired" elements = = timeToLive
+     * @param timeToLive - lifetime of an item in the cache
+     */
+    public FileManager(long timeToLive) {
+        this.timeToLive = timeToLive;
+        this.checkTime = timeToLive;
+    }
+
+    /**
+     * Thread for checking "expired" elements
+     */
+    {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        removeExpired();
+                        Thread.sleep(checkTime);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     /**
      * The method adds an item to the cache
-     *
      * @param key   - the unique key
      * @param value - the value to store
      */
     public void put(K key, V value) {
-        repository.put(new Key(key, timeLeft), value);
+        repository.put(new Key(key, timeToLive), value);
     }
 
     /**
      * The method retrieves an item from the cache
-     *
      * @param key - unique key for getting the value
      * @return an object by key
      */
     public V get(K key) {
-        return repository.get(new Key(key, timeLeft));
+        return repository.get(new Key(key));
     }
 
     /**
      * The method removes an item from the cache
-     *
      * @param key - unique key for getting the value
      */
     public void remove(K key) {
-        repository.remove(new Key(key, timeLeft));
+        repository.remove(new Key(key));
     }
+
+    /**
+     * Deletes items from the cache that have expired storage time
+     */
+    public void removeExpired() {
+        long current = System.currentTimeMillis();
+
+        for (Key k : repository.keySet()) {
+            if (k.isDead(current)) {
+                repository.remove(k);
+            }
+        }
+    }
+
+    /**
+     * Deletes all items from the cache
+     */
+    public void removeAll() {
+        repository.clear();
+    }
+
+    /**
+     * Checks whether the cache contains an item in the collection by key
+     * @param key - unique key for getting the value
+     * @return - true if contains. false if not contain
+     */
+    public boolean containsItem(K key) {
+        removeExpired();
+        V value = get(key);
+        return value != null;
+    }
+
+    /**
+     * Method for updating the time when an item was added.
+     * @param key - unique key for getting the value.
+     * @param dateCreation - time in millis.
+     */
+    public void refreshCreationDate(K key, long dateCreation) {
+        for (Key k : repository.keySet()) {
+            if (k.getKey().equals(key)) {
+                k.refreshDateCreation(dateCreation);
+            }
+        }
+    }
+
+    /**
+     * Method for getting the time when an item was added.
+     * @param key - unique key for getting the value.
+     * @return - creation date in millis.
+     */
+    public long getTimeToLiveForElement(K key) {
+        long dk = 0;
+        for (Key k : repository.keySet()) {
+            if (k.getKey().equals(key)) {
+                dk = k.getDateCreation();
+            }
+        }
+        return dk;
+    }
+
 
     /**
      * @return the number of items in the cache
@@ -48,18 +131,51 @@ public class FileManager<K, V> {
         return repository.size();
     }
 
+    /**
+     * Allows you to change the standard lifetime of an object.
+     * @param timeToLive - time live in millis.
+     */
+    public void setDefaultTimeToLive(long timeToLive) {
+        this.timeToLive = timeToLive;
+    }
+
+    /**
+     * Getter timeToLive for test
+     */
+    public long getTimeToLive() {
+        return timeToLive;
+    }
+
+    /**
+     * Allows you to get the time after which "expired" objects are deleted.
+     * @return checkTime in millis.
+     */
+    public long getCheckTime() {
+        return checkTime;
+    }
+
+    /**
+     * Sets the scan time for the cache to automatically remove the "expired" elements.
+     * @param checkTime - time in millis
+     */
+    public void setCheckTime(long checkTime) {
+        this.checkTime = checkTime;
+    }
 
     private static class Key {
         private final Object key;
         private long lifeTime;
+        private long dateCreation;
 
         /**
          * @param key     - The key by which we get access to the object.
          * @param timeout - The lifetime of the object.
+         * @value dateCreation  - Time when the item was added
          */
         public Key(Object key, long timeout) {
             this.key = key;
-            this.lifeTime = System.currentTimeMillis() + timeout;
+            this.lifeTime = timeout;
+            this.dateCreation = System.currentTimeMillis();
         }
 
         public Key(Object key) {
@@ -71,8 +187,25 @@ public class FileManager<K, V> {
          * false if current time more than lifetime of the object in the cache
          * true  if current time less than lifetime of the object in the cache
          */
-        public boolean isLive(long currentTimeMillis) {
-            return currentTimeMillis < lifeTime;
+        public boolean isDead(long currentTimeMillis) {
+            return currentTimeMillis - dateCreation > lifeTime;
+        }
+
+        /**
+         * Method for updating the time when an item was added.
+         * @param dateCreation - time when an item was added to the cache.
+         */
+        public void refreshDateCreation(long dateCreation) {
+            System.out.println("body dateCreat: " + dateCreation);
+            this.dateCreation = dateCreation;
+        }
+
+        public long getDateCreation() {
+            return dateCreation;
+        }
+
+        public Object getKey() {
+            return key;
         }
 
         @Override
--- src/test/java/testFileManager.java
+++ src/test/java/testFileManager.java
@@ -2,19 +2,91 @@ import com.cinimex.tasks.FileManager;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 public class testFileManager {
 
     @Test
-    public void testAddObjectInCacheAndGetItAndDelete() {
-        FileManager<String, String> cache = new FileManager<>(10000);
-        String testKey = "testKey";
-        String testValue = "testValue";
-
+    public void testAddObjectInCache() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
         Assert.assertEquals(0, cache.size());
-        cache.put(testKey, testValue);
-        Assert.assertEquals(testValue, cache.get(testKey));
+        cache.put("testKey", "testValue");
         Assert.assertEquals(1, cache.size());
-        cache.remove(testKey);
+    }
+
+    @Test
+    public void testAddObjectAndGetIt() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
+        cache.put("testKey", "testValue");
+        Assert.assertEquals("testValue", cache.get("testKey"));
+    }
+
+    @Test
+    public void testAddObjectAndRemoveIt() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
+        cache.put("testKey", "testValue");
+        cache.remove("testKey");
         Assert.assertEquals(0, cache.size());
+    }
+
+    @Test
+    public void testAddSeveralObjectsAndRemoveAll() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
+        for (int i = 0; i < 5; i++) {
+            cache.put("testKey" + i, "testValue" + i);
+        }
+        cache.removeAll();
+        Assert.assertEquals(0, cache.size());
+    }
+
+    @Test
+    public void testDoContainsItem() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
+        cache.put("testKey", "testValue");
+        Assert.assertTrue(cache.containsItem("testKey"));
+    }
+
+    @Test
+    public void testDoContainsItemAfterDelete() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(10));
+        cache.put("testKey", "testValue");
+        cache.remove("testKey");
+        Assert.assertFalse(cache.containsItem("testKey"));
+    }
+
+    /**
+     * Тест на автоматическую проверку элементов кэша и удаление "протухших"
+     */
+    @Test
+    public void testRemoveExpiredElementsIfTimeLiveEnd() throws InterruptedException {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(1));
+        cache.put("testKey", "testValue");
+        System.out.println("testValue exist? " + cache.containsItem("testKey"));
+        Assert.assertTrue(cache.containsItem("testKey"));
+        Thread.sleep(1000);
+        System.out.println("testValue exist? " + cache.containsItem("testKey"));
+        Assert.assertFalse(cache.containsItem("testKey"));
+    }
+
+    @Test
+    public void testSetTimeToLiveInRunTime() {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(1));
+        Assert.assertEquals(1000, cache.getTimeToLive());
+
+        cache.setDefaultTimeToLive(TimeUnit.HOURS.toMillis(1));
+        Assert.assertEquals(TimeUnit.HOURS.toMillis(1), cache.getTimeToLive());
+    }
+
+    @Test
+    public void testUpdatingObjectCreationDate() throws InterruptedException {
+        FileManager<String, String> cache = new FileManager<>(TimeUnit.SECONDS.toMillis(5));
+        cache.put("testKey", "testValue");
+        long firstCreationDate = cache.getTimeToLiveForElement("testKey");
+        Thread.sleep(10);
+        Assert.assertTrue(firstCreationDate < System.currentTimeMillis());
+        cache.refreshCreationDate("testKey", System.currentTimeMillis());
+        long secondCreationDate = cache.getTimeToLiveForElement("testKey");
+        Thread.sleep(10);
+        Assert.assertTrue(secondCreationDate < System.currentTimeMillis());
     }
 }
